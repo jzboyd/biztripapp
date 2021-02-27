33cbf84a8218a4759fccd76596273e626314796b
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-02-27 11:56:09 -0600

    list of props

--- App.js
+++ App.js
@@ -1,33 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Entype from 'react-native-vector-icons/Entypo'
 
-import HomeScreen from './src/screens/Home'
-import Post from './src/components/Post'
+import HomeScreen from './src/screens/Home';
+import SearchResultsScreen from './src/screens/SearchResults';
+import Post from './src/components/Post';
 
 import feed from './assets/data/feed'
 
 const post1 = feed[0];
+const post2 = feed[2];
 
 export default function App() {
   return (
     <>
-    <StatusBar style="auto" />
+    <StatusBar barStyle="auto" />
     <SafeAreaView>
+    
      {/* <HomeScreen /> */}
-     <Post post={post1} />
-      
+     {/* <Post post={post1} /> */}
+     <SearchResultsScreen />
+    
     </SafeAreaView>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
